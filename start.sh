0ee951aaa3a73848f9d53d30bac30c71acf11483
Abhishek <117830155+abhithemachinegun@users.noreply.github.com>
2024-05-24 10:48:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/abhithemachinegun/URLShorterner.git /URLShorterner
-cd /URLShortener
+cd /URLShorterner
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
